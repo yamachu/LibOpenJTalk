19417bb67fab7c1f40d3a614b231cfc912ab2a48
Yusuke Yamada <yamachu.dev@gmail.com>
2017-07-28 14:03:27 +0900

    Fix script

--- build_windows.bat
+++ build_windows.bat
@@ -2,13 +2,6 @@ cd /d %~dp0
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 
-git clone https://github.com/yamachu/hts_engine_API.git
-cd hts_engine_API
-./setup_windows.bat
-
-cd ..
-@cd
-
 nmake /f Makefile.mak
 
 echo Finish OpenJTalk library build task
---
+++ setup_windows.bat
@@ -0,0 +1,9 @@
+cd /d %~dp0
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+
+git clone https://github.com/yamachu/hts_engine_API.git
+cd hts_engine_API
+setup_windows.bat
+
+echo Setup Finished
