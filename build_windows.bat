b2501cb356ce0aa463042c8ad3bff5d44a89a602
Yusuke Yamada <yamachu.dev@gmail.com>
2017-09-20 19:39:14 +0900

    Build multi arch for Windows

--- build_windows.bat
+++ build_windows.bat
@@ -1,7 +1,13 @@
 cd /d %~dp0
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+if %ARCH% == x86 (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+) else (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+)
 
 nmake /f Makefile.mak
+
+ren library\open_jtalk.dll %ARCH%_open_jtalk.dll
 
 echo Finish OpenJTalk library build task
--- setup_windows.bat
+++ setup_windows.bat
@@ -1,6 +1,10 @@
 cd /d %~dp0
 
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+if %ARCH% == x86 (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+) else (
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+)
 
 git clone https://github.com/yamachu/hts_engine_API.git
 cd hts_engine_API
