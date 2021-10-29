9361c2d352cbc2e40a5ab545be93faabe3abf3cb
yamachu <yamachu.dev@gmail.com>
2021-10-29 19:20:08 +0900

    ...

--- build_wasm.sh
+++ build_wasm.sh
@@ -7,13 +7,13 @@ git submodule update --init
 pushd hts_engine_API
 chmod +x setup.sh
 emconfigure ./configure
-make -j4
+emmake make -j4
 popd
 
 # build OpenJTalk
 emconfigure ./configure --with-hts-engine-header-path=`pwd`/hts_engine_API/include --with-hts-engine-library-path=`pwd`/hts_engine_API/lib --with-charset=UTF-8
 # mecab-naist-jdicでコケるので、それをignore
-make -j || :
+emmake make -j || :
 
-make -C library/lang wasm_static CXX=em++ AR=emar
-make -C library/full wasm_static CXX=em++ AR=emar
+emmake make -C library/lang wasm_static CXX=em++ AR=emar LINK=em++
+emmake make -C library/full wasm_static CXX=em++ AR=emar LINK=em++
