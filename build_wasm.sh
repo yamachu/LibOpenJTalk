9c81e13d87c54c890aba6aa15e2cc6735ff67120
yamachu <yamachu.dev@gmail.com>
2025-11-28 21:02:46 +0900

    ...

--- build_wasm.sh
+++ build_wasm.sh
@@ -6,12 +6,12 @@ git submodule update --init
 # initialize hts_engine_API
 pushd hts_engine_API
 chmod +x setup.sh
-emconfigure ./configure --host=wasm32-unknown-emscripten
+emconfigure ./configure --host=wasm32-unknown-none
 emmake make -j4
 popd
 
 # build OpenJTalk
-emconfigure ./configure --host=wasm32-unknown-emscripten --with-hts-engine-header-path=`pwd`/hts_engine_API/include --with-hts-engine-library-path=`pwd`/hts_engine_API/lib --with-charset=UTF-8
+emconfigure ./configure --host=wasm32-unknown-none --with-hts-engine-header-path=`pwd`/hts_engine_API/include --with-hts-engine-library-path=`pwd`/hts_engine_API/lib --with-charset=UTF-8
 emmake make -j
 
 emmake make -C library/lang wasm_static CXX=em++ AR=emar LINK=em++
