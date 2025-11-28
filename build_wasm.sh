bb9c9bf9b25b71f946f11a1b6afe27fe92ceaa51
yamachu <yamachu.dev@gmail.com>
2025-11-28 21:07:29 +0900

    ファイルからregisterキーワードを消せば楽だけど、一旦オプションで黙らせる

--- build_wasm.sh
+++ build_wasm.sh
@@ -14,5 +14,5 @@ popd
 emconfigure ./configure --host=wasm32-unknown-none --with-hts-engine-header-path=`pwd`/hts_engine_API/include --with-hts-engine-library-path=`pwd`/hts_engine_API/lib --with-charset=UTF-8
 emmake make -j
 
-emmake make -C library/lang wasm_static CXX=em++ AR=emar LINK=em++
-emmake make -C library/full wasm_static CXX=em++ AR=emar LINK=em++
+emmake make -C library/lang wasm_static CXX=em++ AR=emar LINK=em++ CXXFLAGS="-O2 -Wall -finput-charset=UTF-8 -fexec-charset=UTF-8 -Wno-register"
+emmake make -C library/full wasm_static CXX=em++ AR=emar LINK=em++ CXXFLAGS="-O2 -Wall -finput-charset=UTF-8 -fexec-charset=UTF-8 -Wno-register"
