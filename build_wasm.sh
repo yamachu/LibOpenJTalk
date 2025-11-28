ed3d15413e816d0b081356cb0690f8024dc9a9e0
yamachu <yamachu.dev@gmail.com>
2025-11-28 20:13:45 +0900

    dictをビルドしないようになったので、失敗は無視しない

--- build_wasm.sh
+++ build_wasm.sh
@@ -12,8 +12,7 @@ popd
 
 # build OpenJTalk
 emconfigure ./configure --with-hts-engine-header-path=`pwd`/hts_engine_API/include --with-hts-engine-library-path=`pwd`/hts_engine_API/lib --with-charset=UTF-8
-# mecab-naist-jdicでコケるので、それをignore
-emmake make -j || :
+emmake make -j
 
 emmake make -C library/lang wasm_static CXX=em++ AR=emar LINK=em++
 emmake make -C library/full wasm_static CXX=em++ AR=emar LINK=em++
